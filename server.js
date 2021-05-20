f287385810bf7815d81e1fd1f35d2c5e2f71f72f
Glitch (glitch-hello-node) <none>
2021-05-20 19:48:11 +0000

    🌇🐜 Checkpoint ./server.js:32019472/5309 ./package.json:32019472/1484

--- server.js
+++ server.js
@@ -1,54 +1,24 @@
-var express = require('express')
-var app = express()
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
-
-const fastify = require("fastify")({
-  logger: false
-});
+const express = require("express");
+const app = express();
 
 const path = require("path");
 let userAccessMap = new Map();
 
-fastify.register(require('fastify-socket.io'), {
+app.use(express.static("public"));
 
-})
-
-
-
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
-
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
-
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
-});
-
-
-// Our home page route, this pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
-  fastify.io.emit('hello')
-
-  let id = request.ip;
+app.get("/", (request, response) => {
   let lastAccess = Date.now();
-  userAccessMap.set(id, lastAccess);
   let activeUserCount = userAccessMap.size
   let params = { activeUserCount: activeUserCount};
-  console.log(params)
-  reply.view("/src/pages/index.hbs", params);
-
+  console.log(request)
+  request.query.acticeUserCount = activeUserCount
+  response.sendFile(__dirname + "/src/pages/index.html", params);
 });
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
 
 const cleanupFrequency = 10 * 1000;    
 const cleanupTarget = 60 * 1000;   
@@ -62,14 +32,3 @@ setInterval(() => {
     }
     console.log('clear', userAccessMap)
 }, cleanupFrequency);
-
-
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
