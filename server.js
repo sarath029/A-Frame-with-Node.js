8b6c5650cc5b58743e264b2936164f85d22e2cd7
Glitch (glitch-hello-node) <none>
2021-05-20 19:59:19 +0000

    🍭🏚 Checkpoint ./server.js:32019472/3114 ./src/pages/index.html:32019472/3108 ./package.json:32019472/1141

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const path = require("path");
-let userAccessMap = new Map();
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-  let lastAccess = Date.now();
-  let activeUserCount = userAccessMap.size
-  let params = { activeUserCount: activeUserCount};
-  console.log(request)
-  request.query.acticeUserCount = activeUserCount
-  response.sendFile(__dirname + "/src/pages/index.html", params);
+  response.sendFile(__dirname + "/src/pages/index.html");
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-
-const cleanupFrequency = 10 * 1000;    
-const cleanupTarget = 60 * 1000;   
-
-setInterval(() => {
-    let now = Date.now();
-    for (let [id, lastAccess] of userAccessMap.entries()) {
-        if (now - lastAccess > cleanupTarget) {
-            userAccessMap.delete(id);
-        }
-    }
-    console.log('clear', userAccessMap)
-}, cleanupFrequency);
