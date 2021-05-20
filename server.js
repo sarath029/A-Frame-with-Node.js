66d483735dc5c389106310e6d645ab41e6fd0ba3
Glitch (glitch-hello-node) <none>
2021-05-20 20:32:51 +0000

    ⏱🌛 Checkpoint ./server.js:32019472/5633

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
-var express = require('express');
-var socket = require('socket.io');
-var app = express();
-var server = app.listen(1111, function(){ console.log("Server Listening"); });
-var io = socket(server);
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+var activeUsers = 0;
 
-// Set up 'public' folder 
-app.use(express.static(path.join(__dirname, 'public')))
+// Set up 'public' folder
+app.use(express.static("public"));
 
 // Point / to index.html (could just put index.html in public but leaving for reference)
-app.use('/', function(req, res, next){
-  res.sendFile('src/pages/index.html', { root : __dirname })
-})
+app.use("/", function(req, res, next) {
+  res.sendFile("src/pages/index.html", { root: __dirname });
+});
 
-// Create new websocket 
-io.sockets.on('connection', function (socket) {
+io.on("connection", socket => {
+  activeUsers++;
+  console.log("a user connected");
+  console.log('active: ', activeUsers);
+  
+  socket.on("disconnect", function() {
+    activeUsers--;
+    console.log("a user disconnected");
+    console.log('active: ', activeUsers);
+  });
   
 });
+
+server.listen(process.env.PORT);
+
+// // listen for requests :)
+// const listener = app.listen(process.env.PORT, () => {
+//   console.log("App is listening on port " + listener.address().port);
+// });
