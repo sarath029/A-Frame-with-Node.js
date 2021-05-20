55813bcbec459a892fea978e4ac70dd0b2ab7077
Glitch (glitch-hello-node) <none>
2021-05-20 21:18:58 +0000

    👡🍨 Checkpoint ./server.js:32019472/1121 ./src/pages/index.ejs:32019472/199

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-io.path("/admin");
+// io.path("/admin");
 
 var bodyParser = require('body-parser');
 var activeUsers = 0;
